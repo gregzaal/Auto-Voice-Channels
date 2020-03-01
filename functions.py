a8fa24c29bea01c66b03b66dbc5a68220a161653
gregzaal <gregzzmail@gmail.com>
2020-03-01 19:09:15 +0200

    Allow patreon authentication via DM

--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
 import asyncio
+import os
 import traceback
 from datetime import datetime
 from copy import deepcopy
 from math import ceil
-from pprint import pformat
 from random import choice, seed
 from statistics import mean
 from time import time
@@ -465,9 +465,9 @@ async def admin_log(msg, client, important=False):
 async def log_timings(client, highlight):
     text = ""
     if highlight is not None:
-        text = "**{0}** took {1:.2f}s\n".format(highlight, cfg.TIMINGS[highlight])
+        text = "**{0}** took {1:.2f}s".format(highlight, cfg.TIMINGS[highlight])
     log(text.replace("**", ""))
-    text += utils.format_timings()
+    text += "\n" + utils.format_timings()
     await admin_log(text, client)
 
 
@@ -591,6 +591,43 @@ def get_sapphire_id(guild):
         if guild.id in sv["servers"]:
             return int(s)
     return None
+
+
+@utils.func_timer()
+async def power_overwhelming(ctx, guild):
+    author = ctx['message'].author
+    r = "Checking..."
+    s = False
+    m = await ctx['channel'].send(r)
+
+    if patreon_info is None:
+        return False, "No need to do that."
+
+    patrons = patreon_info.fetch_patrons(force_update=False)
+    patrons[cfg.CONFIG['admin_id']] = "sapphire"
+    auth_path = os.path.join(cfg.SCRIPT_DIR, "patron_auths.json")
+    if author.id in patrons:
+        auths = utils.read_json(auth_path)
+        auths[str(author.id)] = {"servers": [guild.id]}
+        utils.write_json(auth_path, auths, indent=4)
+        patreon_info.update_patron_servers(patrons)
+        s = True
+        reward = patrons[author.id].title()
+        await admin_log("🔑 Authenticated **{}**'s {} server {} `{}`".format(
+            author.name, reward, guild.name, guild.id
+        ), ctx['client'], important=True)
+        r = "✅ Nice! This server is now a **{}** server.".format(reward)
+        if reward in ["Diamond", "Sapphire"]:
+            r += ("\nPlease give me ~{} hours to set up your private bot - "
+                  "I'll DM you when it's ready to make the swap!".format(12 if reward == "Sapphire" else 24))
+    else:
+        await admin_log("🔒 Failed to authenticate for **{}**".format(author.name), ctx['client'])
+        r = ("❌ Sorry it doesn't look like you're a Patron.\n"
+             "If you just recently became one, please make sure you've connected your discord account "
+             "(<https://bit.ly/2UdfYbQ>) and try again in a few minutes. "
+             "If it still doesn't work, let me know in the support server: <https://discord.io/DotsBotsSupport>.")
+    await m.edit(content=r)
+    return s, "NO RESPONSE"
 
 
 @utils.func_timer()
