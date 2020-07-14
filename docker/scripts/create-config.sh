fd792b7106d5a8a34b99fb70b1ea063df985b346
charles ragg <chazragg@gmail.com>
2020-07-14 14:45:29 +0100

    Created a shell scripts to setp config.json

--- docker/scripts/create-config.sh
+++ docker/scripts/create-config.sh
@@ -1,2 +1,2 @@
-json='{"admin_id": $admin_id, "client_id": $client_id, "log_timezone": "$log_timezone","token": "$token"}'
+json="{\"admin_id\": \"${admin_id}\", \"client_id\": \"${client_id}\", \"log_timezone\": \"${log_timezone}\",\"token\": \"${token}\"}"
 echo $json > config.json
